6bb61012d8442361f1cc28a7ad6de4ebfb91e508
lucasquinalia <lrgquinalia@gmail.com>
2025-08-20 17:06:45 -0300

    focus class creation

---
+++ src/main/java/co/lockinfocus/lockin/focus/Focus.java
@@ -0,0 +1,34 @@
+package co.lockinfocus.lockin.focus;
+
+import co.lockinfocus.lockin.task.Task;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Table(name = "focuses")
+@Entity(name = "Focus")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Focus {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int timer;
+    private int short_break;
+    private int long_break;
+    @Embedded
+    private List<Task> tasks;
+
+    public Focus(DataFocus data) {
+        this.title = data.title();
+        this.timer = data.timer();
+        this.short_break = data.short_break();
+        this.long_break = data.long_break();
+    }
+}
