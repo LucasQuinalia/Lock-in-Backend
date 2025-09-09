c41313047c69779ad0e8bb7efdad2373fcd044f1
lucasquinalia <lrgquinalia@gmail.com>
2025-09-09 20:23:44 -0300

    task get and get by id methods implementation

--- src/main/java/co/lockinfocus/lockin/controller/TaskController.java
+++ src/main/java/co/lockinfocus/lockin/controller/TaskController.java
@@ -9,6 +9,9 @@ import co.lockinfocus.lockin.task.TaskRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -37,6 +40,25 @@ public class TaskController {
             var uri = uriBuilder.path("/focuses/" + focusId + "/tasks/{id}").buildAndExpand(task.getId()).toUri();
 
             return ResponseEntity.created(uri).body(new DataGetTask(task));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DataGetTask>> read(@PageableDefault(size = 15, sort = {"title"}) Pageable pageable) {
+        var page = taskRepository.findAll(pageable).map(DataGetTask::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataGetTask> readById(@PathVariable Long id) {
+        var optionalTask = taskRepository.findById(id);
+
+        if (optionalTask.isPresent()) {
+            var task = optionalTask.get();
+            return ResponseEntity.ok(new DataGetTask(task));
         }
 
         return ResponseEntity.notFound().build();
